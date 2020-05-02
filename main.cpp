bea1361a54eed1f1a6d4f232bbfaabe865ad9130
paseam <zhucongxian@qq.com>
2020-05-02 13:00:13 +0800

    heap_sort completed

--- main.cpp
+++ main.cpp
@@ -1,11 +1,11 @@
 #include<iostream>
 #include<vector>
-#include "bubble_sort.h"
+#include "heap_sort.h"
 
 int main()
 {
-    std::vector<int> data={4,1,5,3,2,6,8,9};
-    bubble_sort(data);
+    std::vector<int> data={};
+    heap_sort(data);
     for(int i=0;i<data.size();i++)
     {
         std::cout<<data[i]<<" ";
--- sort_algorithm/heap_sort.h
+++ sort_algorithm/heap_sort.h
@@ -1,7 +1,73 @@
+#ifndef _HEAP_SORT_
+#define _HEAP_SORT_
+
 #include <vector>
 #include <memory>
 
-int main()
+
+static int swap(std::vector<int>& data, int i,int j)
 {
-    
+    int temp=data[i];
+    data[i]=data[j];
+    data[j]=temp;
+    return 0;
 }
+static int heapify(std::vector<int>& data, int index, int n)
+{
+    //int n=data.size();
+    int left=2*index+1;
+    int right=2*index+2;
+
+    if(left>=n && right>=n)
+        return 0;
+    
+    int largest=index;
+    if(left<n)
+    {
+        if(data[left]>data[largest])
+            largest=left;
+    }
+
+    if(right<n)
+    {
+        if(data[right]>data[largest])
+            largest=right;
+    }
+
+    if(largest!=index)
+    {
+        //swap 
+        swap(data,largest,index);
+        heapify(data,largest,n);
+    }
+    return 0;
+}
+
+
+
+static int build_heap(std::vector<int>& data)
+{
+    for(int i=data.size()/2;i>=0;i--)
+    {
+        heapify(data,i,data.size());
+    }
+    return 0;
+}
+
+
+int heap_sort(std::vector<int>& data)
+{
+    if(data.size()==0)
+        return 0;
+    //build heap
+    build_heap(data);
+    //sort data 
+    int nLen=data.size();
+    for(int i=0;i<data.size()-1;i++)
+    {
+        swap(data,0,data.size()-1-i);
+        heapify(data,0,--nLen);
+        //nLen--;
+    }
+}
+#endif //_HEAP_SORT_
